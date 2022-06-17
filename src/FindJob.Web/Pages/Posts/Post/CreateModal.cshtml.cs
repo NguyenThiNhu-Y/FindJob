1308b1b9b43a6f7ea317d9dc08b4fda11c728c34
NguyenThiNhu-Y <82461929+NguyenThiNhu-Y@users.noreply.github.com>
2022-06-18 00:10:07 +0700

    .

--- src/FindJob.Web/Pages/Posts/Post/CreateModal.cshtml.cs
+++ src/FindJob.Web/Pages/Posts/Post/CreateModal.cshtml.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using FindJob.Posts;
 using FindJob.Posts.Dtos;
 using FindJob.Web.Pages.Posts.Post.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using FindJob.Fields;
+using System.Linq;
 
 namespace FindJob.Web.Pages.Posts.Post
 {
@@ -10,12 +14,39 @@ namespace FindJob.Web.Pages.Posts.Post
     {
         [BindProperty]
         public CreateEditPostViewModel ViewModel { get; set; }
+        public List<SelectListItem> ListIdParent { get; set; }
+
 
         private readonly IPostAppService _service;
+        private readonly IFieldRepository _fieldRepository;
 
-        public CreateModalModel(IPostAppService service)
+
+        public CreateModalModel(IPostAppService service, IFieldRepository fieldRepository)
         {
             _service = service;
+            _fieldRepository = fieldRepository;
+        }
+        public virtual async Task OnGetAsync()
+        {
+            ListIdParent = new List<SelectListItem>();
+            ListIdParent.Add(new SelectListItem
+            {
+                Text = L["Choose"],
+                Value = ""
+            });
+            var items = (await _fieldRepository.GetListAsync());
+            if (items.Count > 0)
+            {
+                ListIdParent.AddRange(
+                (items.Select(t => new SelectListItem
+                {
+                    Text = t.Name,
+                    Value = t.Id.ToString()
+                }).ToList()
+            ));
+            }
+
+
         }
 
         public virtual async Task<IActionResult> OnPostAsync()
