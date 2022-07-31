1f240601a059f0586e540278eab854c5799236b2
NguyenThiNhu-Y <82461929+NguyenThiNhu-Y@users.noreply.github.com>
2022-07-31 19:39:38 +0700

    update file index

--- src/FindJob.Web/Pages/Index.cshtml.cs
+++ src/FindJob.Web/Pages/Index.cshtml.cs
@@ -5,14 +5,9 @@ namespace FindJob.Web.Pages
 {
     public class IndexModel : FindJobPageModel
     {
-        public async Task<IActionResult> OnGet()
+        public async Task OnGet()
         {
-            var currentUser = CurrentUser.Id;
-            if (currentUser == null)
-            {
-                return Redirect("/Client/Index");
-            }
-            return Page();
+            
         }
     }
 }
